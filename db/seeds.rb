41650019e0ed414719844b12157c325b5ef37bb0
naaltunian <naaltunian@icloud.com>
2018-08-27 10:29:56 -0400

    create Place model and seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.find_or_create_by(
+  name: "Miami Beach Senior High",
+  street: "2231 Prairie Ave",
+  city: "Miami Beach"
+)
+sleep 1
+Place.find_or_create_by(
+  name: "Dali Museum",
+  street: "1 Dali Blvd",
+  city: "St Petersburg",
+  state: "FL"
+)
+sleep 1
+Place.find_or_create_by(
+  name: "Joe's Stone Crab",
+  street: "11 Washington Ave",
+  city: "Miami Beach"
+)
+sleep 1
+Place.find_or_create_by(
+  name: "Wyncode",
+  street: "549 NW 28th Street",
+  city: "Miami",
+  state: "Florida"
+)
